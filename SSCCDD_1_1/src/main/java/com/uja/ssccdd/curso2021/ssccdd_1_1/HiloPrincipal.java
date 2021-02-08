5de8f493da28474da52c935c196632ce7d7c4a51
Alberto <aexpositogarcia@outlook.com>
2021-02-08 17:24:16 +0100

    Creación de clases

---
+++ SSCCDD_1_1/src/main/java/com/uja/ssccdd/curso2021/ssccdd_1_1/HiloPrincipal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uja.ssccdd.curso2021.ssccdd_1_1;
+
+/**
+ *
+ * @author alber
+ */
+public class HiloPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ SSCCDD_1_1/src/main/java/com/uja/ssccdd/curso2021/ssccdd_1_1/Vendedor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uja.ssccdd.curso2021.ssccdd_1_1;
+
+/**
+ *
+ * @author alber
+ */
+public class Vendedor {
+    
+}
